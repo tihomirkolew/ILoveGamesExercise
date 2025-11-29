0e1a5a45cf2f96b0564ae6c27313dbb9ac0b23cb
Tihomir Kolev <sineadfly@gmail.com>
2025-11-29 13:02:55 +0200

    chore: update .gitignore to include firebaseConfig.js and refactor firebase.js to import configuration from a separate file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+
+import { firebaseConfig } from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDdKMCK_2DABzVdm1XYdT9VYckzmZbzrXA",
-  authDomain: "react-learning-softuni.firebaseapp.com",
-  projectId: "react-learning-softuni",
-  storageBucket: "react-learning-softuni.firebasestorage.app",
-  messagingSenderId: "446101339768",
-  appId: "1:446101339768:web:5fee8bb69aedb4dd3c378d"
-};
+// config in another file
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
