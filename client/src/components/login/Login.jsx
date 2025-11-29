e4477130f3c4d8ff4a91bd772d8427ffe4c9fd6c
Tihomir Kolev <sineadfly@gmail.com>
2025-11-29 15:39:02 +0200

    feat: refactor Login component to use useForm hook for form handling and integrate UserContext for authentication

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,24 +1,19 @@
 import { useNavigate } from "react-router";
+import useForm from "../../hooks/useForm";
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
 
-export default function Login({
-    onLogin,
-}) {
+export default function Login() {
     const navigate = useNavigate();
+    const { loginHandler } = useContext(UserContext);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.target)
-
-        const email = formData.get('email');
-        const password = formData.get('password');
-
+    const submitHandler = async ({email, password}) => {
         if (!email || !password) {
             return alert('Email and password are required!');
         }
 
         try {
-            onLogin(email, password);
+            await loginHandler(email, password);
 
             navigate('/');
         } catch (err) {
@@ -26,18 +21,27 @@ export default function Login({
         }
     }
 
+    const {
+        register,
+        formAction,
+
+    } = useForm(submitHandler, {
+        email: '',
+        password: '',
+    });
+
     return (
         <>
             <section id="login-page">
 
-                <form id="login" onSubmit={submitHandler}>
+                <form id="login" action={formAction}>
                     <div className="container">
                         <h1>Login</h1>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Your Email" />
+                        <input type="email" id="email" {...register('email')} placeholder="Your Email" />
 
                         <label htmlFor="login-pass">Password</label>
-                        <input type="password" id="login-password" name="password" placeholder="Password" />
+                        <input type="password" id="login-password" {...register('password')} placeholder="Password" />
                         <input type="submit" className="btn submit" value="Login" />
                     </div>
                 </form>
