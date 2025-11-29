517768cf922ab525de748c150bc87454bb6dd774
Tihomir Kolev <sineadfly@gmail.com>
2025-11-29 22:39:54 +0200

    feat: enhance header component to conditionally render user links based on authentication status

--- client/src/components/createGame/CreateGame.jsx
+++ client/src/components/createGame/CreateGame.jsx
@@ -1,28 +1,15 @@
 import { useNavigate } from "react-router";
-import request from "../../utils/request";
-import { useEffect, useState } from "react";
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import { storage } from "../../firebase";
+import useForm from "../../hooks/useForm";
+import useRequest from "../../hooks/useRequest";
 
 export default function CreateGame() {
 
     const navigate = useNavigate();
-    const [imageUpload, setImageUpload] = useState(false);
-    const [imagePreview, setImagePreview] = useState(null);
+    const { request } = useRequest();
 
-    useEffect(() => {
-        return () => {
-            URL.revokeObjectURL(imagePreview);
-            setImagePreview(null);
-        }
-    }, [imageUpload]);
+    const createGameHandler = async (values) => {
 
-    const createGameHandler = async (e) => {
-        e.preventDefault();
 
-        const formData = new FormData(e.target);
-
-        const { image, ...data } = Object.fromEntries(formData);
 
         // const response = await fetch("http://localhost:3030/jsonstore/games", {
         //     method: 'POST',
@@ -34,80 +21,70 @@ export default function CreateGame() {
 
         // const result = await response.json();
 
-        if (imageUpload) {
-            // todo upload image
-            const imageRef = ref(storage, `images/${image.name}`);
-            await uploadBytes(imageRef, image);
-            data.imageUrl = await getDownloadURL(imageRef);
-        } else {
-            data.imageUrl = image;
-        }
-        
+        const data = values;
+
         data.players = Number(data.players);
         data._createdOn = Date.now();
 
-        await request('/games', 'POST', data);
+        try {
+            await request('/data/games', 'POST', data);
 
-        navigate('/games');
+            navigate('/games');
+        } catch (error) {
+            alert(error.message)
+        }
     }
 
-    const imageChangeHandler = (e) => {
-        const image = e.target.files[0];
-
-        const imageUrl = URL.createObjectURL(image);
-
-        setImagePreview(imageUrl);
-
-    }
-
-    const imageClickUploadHandler = () => {
-        setImageUpload(state => !state);
-    }
+    const {
+        register,
+        formAction
+    } = useForm(createGameHandler, {
+        title: '',
+        genre: '',
+        players: '',
+        date: '',
+        imageUrl: '',
+        summary: '',
+    })
 
     return (
         <>
             <section id="add-page">
-                <form id="add-new-game" onSubmit={createGameHandler}>
+                <form id="add-new-game" action={formAction}>
                     <div className="container">
 
                         <h1>Add New Game</h1>
 
                         <div className="form-group-half">
                             <label htmlFor="gameName">Game Name:</label>
-                            <input type="text" id="gameName" name="title" placeholder="Enter game title..." />
+                            <input type="text" id="gameName" {...register('title')} placeholder="Enter game title..." />
                         </div>
 
                         <div className="form-group-half">
                             <label htmlFor="genre">Genre:</label>
-                            <input type="text" id="genre" name="genre" placeholder="Enter game genre..." />
+                            <input type="text" id="genre" {...register('genre')} placeholder="Enter game genre..." />
                         </div>
 
                         <div className="form-group-half">
                             <label htmlFor="activePlayers">Active Players:</label>
-                            <input type="number" id="activePlayers" name="players" min="0" placeholder="0" />
+                            <input type="number" id="activePlayers" {...register('players')} min="0" placeholder="0" />
                         </div>
 
                         <div className="form-group-half">
                             <label htmlFor="releaseDate">Release Date:</label>
-                            <input type="date" id="releaseDate" name="date" />
+                            <input type="date" id="releaseDate" {...register('date')} />
                         </div>
 
                         <div className="form-group-full">
-                            <label htmlFor="image">Image {imageUpload ? 'Upload' : 'URL'}:</label>
-                            <button type="button" className="details-button" onClick={imageClickUploadHandler}>{imageUpload ? 'Url' : 'Image Upload'}</button>
-                            {imageUpload
-                                ? <input type="file" id="image" name="image" placeholder="Upload file..." onChange={imageChangeHandler} />
-                                : <input type="text" id="image" name="image" placeholder="Enter image URL..." />
-                            }
-                            {imagePreview && (
-                                <img src={imagePreview} alt="preview image" />
-                            )}
+                            <label htmlFor="image">Image URL:</label>
+                            <input type="text" id="image" {...register('imageUrl')} placeholder="Enter image URL..." />
+
 
                         </div>
 
                         <div className="form-group-full">
                             <label htmlFor="summary">Summary:</label>
-                            <textarea name="summary" id="summary" rows="5" placeholder="Write a brief summary..."></textarea>
+                            <textarea {...register('summary')} id="summary" rows="5" placeholder="Write a brief summary..."></textarea>
                         </div>
 
                         <input className="btn submit" type="submit" value="ADD GAME" />
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,20 +1,29 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import UserContext from "../contexts/UserContext";
 
 export default function Header() {
-    
+    const { isAuthenticated } = useContext(UserContext);
+
     return (
         <header>
             <nav>
                 <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /> </Link>
                 <Link to="/games">Catalog</Link>
-                <div id="user">
-                    <Link to="/games/create">Add Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {isAuthenticated
+                    ? (
+                        <div id="user">
+                            <Link to="/games/create">Add Game</Link>
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }
             </nav>
         </header>
 
